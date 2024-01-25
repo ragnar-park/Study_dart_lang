580e9d6839b8e7ed5643342122822934ea51c14c
ragnar <mystparkkorea@gmail.com>
2024-01-25 22:50:31 +0900

    1.4.1 리스트 문법

--- lib/1.4/1_list_type.dart
+++ lib/1.4/1_list_type.dart
@@ -1,1 +1,16 @@
-// temp
+void main() {
+  // 리스트에 넣을 타입을 <> 사이에 명시 가능
+  List<String> soccerPlayerList = ['호날두', '메시', '손흥민', '즐라탄'];
+
+  print(soccerPlayerList);
+  print(soccerPlayerList[0]);
+  print(soccerPlayerList[1]);
+  print(soccerPlayerList[2]);
+  print(soccerPlayerList[3]);
+
+  print(soccerPlayerList.length);
+
+  soccerPlayerList[1] = '홀란드';
+
+  print(soccerPlayerList);
+}
